c9e7c30141ba3823ae810fccf18d6464a0a4ff02
Kevin <kevin@Kevins-MBP.fios-router.home>
2019-02-02 04:09:12 -0500

    check src/components + src/ to write file

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,35 @@ import { Files, Parser, Transformer } from './lib'
         const componentClass = Transformer.createFunctionalComponent(componentName)
         const componentTest = Transformer.createRenderTest(componentName)
         
-        const path = Files.getPath(pathParts, componentName, 'js')
-        const testPath = Files.getPath([...pathParts, '__tests__'], componentName, 'js')
+        const cwd = process.cwd()
 
-        /*
-        await Files.writeFile(path, componentClass)
-        await Files.writeFile(testPath, componentTest)
-        */
+        const cwdDir = [cwd, ...pathParts]
+        const srcDir = [cwd, 'src', ...pathParts]
+        const srcComponentsDir = [cwd, 'src', 'components', ...pathParts]
+
+        let dirToUse: string[]
         
+        //Check for component folder first 
+        if(await Files.dirExists(srcComponentsDir)) {
+            dirToUse = srcDir
+        } else if(await Files.dirExists(srcDir)) {
+            dirToUse = srcComponentsDir
+        } else {
+            dirToUse = cwdDir
+        }
+
+        createComponent(dirToUse, componentName, componentClass, componentTest)
     } catch(e) {
         console.warn(e)
 
         process.exit(1)
     }
 })(process.argv)
+
+async function createComponent(componentPath: string[], componentName: string, componentClass: string, componentTest: string) {
+    const path = Files.getPath(componentPath, componentName, 'js')
+    const testPath = Files.getPath([...componentPath, '__tests__'], componentName, 'js')
+
+    await Files.writeFile(path, componentClass)
+    await Files.writeFile(testPath, componentTest)
+}
--- src/lib/files.ts
+++ src/lib/files.ts
@@ -1,6 +1,24 @@
 import fs from 'fs'
 import path from 'path'
 
+export function dirExists(parts: string[]): Promise<boolean> {
+    try{
+        const pathName = path.resolve(...parts)
+
+        return new Promise((resolve, reject) => {
+            try {
+                fs.exists(pathName, exists => {
+                    resolve(exists)
+                })
+            } catch(e) {
+                reject(e)
+            }
+        })
+    } catch(e) {
+        return Promise.reject(e)
+    }
+}
+
 export function getPath(parts: string[], filename: string, extension: string): string {
     const file = `${filename}.${extension}`
 
@@ -10,12 +28,16 @@ export function getPath(parts: string[], filename: string, extension: string): s
 }
 
 export function writeFile(path: string, content: string): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(path, content, err => {
-            if(err)
-                reject(err)
-            
-            resolve(true)
+    try {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path, content, err => {
+                if(err)
+                    reject(err)
+                
+                resolve(true)
+            })
         })
-    })
+    } catch(e) {
+        return Promise.reject(e)
+    }
 }
