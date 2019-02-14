ba7e787b3c9462e423f3ee09ff58089b9df4b94d
Kevin Diem <kgd003@apca938.833chestnut.tju.edu>
2019-02-13 23:42:42 -0500

    use webpack

--- bin/templates/imports.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REACT = 'import React from \'react\'';
-exports.REACT_WITH_COMPONENT = 'import React, { Component } from \'react\'';
-exports.PROP_TYPES = 'import PropTypes from \'prop-types\'';
-exports.TESTS = {
-    SHALLOW: 'import { shallow } from \'enzyme\''
-};
--- bin/templates/tests.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DESCRIBE = function (componentName, assertion) { return ("describe('" + componentName + "', () => {" + assertion + "})"); };
-exports.IT = function (testDescription, testBody) { return ("\n  it('" + testDescription + "', () => {\n    " + testBody + "\n  })\n"); };
-exports.SHALLOW_RENDER = function (componentName) { return "shallow(<" + componentName + "/>)"; };
-exports.TEST = function (componentName, testDescription, testBody) { return (exports.DESCRIBE(componentName, exports.IT(testDescription, testBody))); };
-exports.RENDER_TEST = function (componentName) { return (exports.TEST(componentName, 'renders without crashing', exports.SHALLOW_RENDER(componentName))); };
